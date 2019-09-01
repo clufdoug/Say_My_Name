c6e65f068b36dcf388d7bbd00ea9832b27f6a374
Doug Cluff <clufdoug@isu.edu>
2019-09-01 14:30:29 -0600

    Created Say_My_Name

---
+++ Say_My_Name.vb
@@ -0,0 +1,17 @@
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("What is your name?")
+        Console.Write("Type your first name ")
+        Dim firstname As String
+
+        firstname = Console.ReadLine()
+
+
+        Console.WriteLine(value:="Hello, " & firstname)
+
+
+
+    End Sub
+
+End Module
